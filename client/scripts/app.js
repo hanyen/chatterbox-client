2c8dbace6a1234d6d2755c655343e8909f43f035
hanyen <me@hanyen.com>
2017-01-02 15:39:19 -0800

    Passed the fetching tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,9 @@
 // YOUR CODE HERE:
 
 //define jquery variables
-var $sendButton = $("sendButton");
-var $textField = $("textField");
-var $chats = $("chats");
+var $submitButton = $(".submit");
+var $message = $("#message");
+var $chats = $("#chats");
 var $chatsContent;
 
 /*
@@ -13,52 +13,92 @@ class can read and write from (via REST).
 
 The message objects you send to the parse server (via POST requests) should be in the following format:
 */
+
+var app = {
+  server: 'https://api.parse.com/1/classes/messages',
+  init: function() {},
+  send: function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: app.server,
+      type: 'GET',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        // $chatsContent = data;
+        // console.log($chatsContent);
+        // $('#chatsTextArea').html($chatsContent.results[0].text);
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to receive message', data);
+      }
+    });
+  }
+};
   
-  var message = {
-    username: 'hanyen',
-    text: 'whazapp!!!',
-    roomname: '4chan'
-  };
+var message = {
+  username: 'hanyen',
+  text: 'whazapp!!!',
+  roomname: 'lobby'
+};
 /*
 To get you started, here's an example POST request. Note that any messages you POST to the server are viewable by everyone, so be nice.
 */
 
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'https://api.parse.com/1/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'https://api.parse.com/1/classes/messages',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });
 
 //create an object named app
 // var App = Parse.Object.extend("App");
 // var app = new App();
 
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'https://api.parse.com/1/classes/messages',
-  type: 'GET',
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'https://api.parse.com/1/classes/messages',
+//   type: 'GET',
   
-  contentType: 'application/json',
-  success: function (data) {
-    $chatsContent = data;
-    console.log($chatsContent);
-    $("#chatsTextArea").html($chatsContent.results[0].text);
-    console.log('chatterbox: Message received');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to receive message', data);
-  }
-});
+//   contentType: 'application/json',
+//   success: function (data) {
+//     $chatsContent = data;
+//     console.log($chatsContent);
+//     $('#chatsTextArea').html($chatsContent.results[0].text);
+//     console.log('chatterbox: Message received');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to receive message', data);
+//   }
+// });
 
 
 /*
